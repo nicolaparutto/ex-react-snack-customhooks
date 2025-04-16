051b2e2c6fe437cfc29065e9384fbec9350e4ca5
nicolaparutto <nicolaparutto2003@gmail.com>
2025-04-16 15:57:23 +0200

    feat: snack 2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import useSwitch from "./hooks/useSwitch"
+import useSwitch from "./hooks/useSwitch";
+import useDate from "./hooks/useDate";
 
 function App() {
 
   const [state, toggle] = useSwitch();
-
+  const { date } = useDate();
   return (
     <>
       <div>
         <h1>Il valore è: {state ? "true" : "false"}</h1>
         <button onClick={toggle}>Cambia valore</button>
+      </div>
+      <div>
+        <h1>Date e ora attuali:</h1>
+        <p>{date.toLocaleString()}</p>
       </div>
     </>
   )
---
+++ src/hooks/useDate.jsx
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react"
+
+function useDate() {
+	const [date, setDate] = useState(new Date());
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setDate(new Date())
+		}, 1000)
+		return () => {
+			clearInterval(interval)
+		}
+	}, [])
+	return { date }
+}
+
+export default useDate
